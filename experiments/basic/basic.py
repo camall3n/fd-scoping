8e7e6eb8873a530d5f392677dd92ac271c8a20ed
Michael Katz <michael.katz1@ibm.com>
2024-10-14 15:41:26 -0400

    basic experiment

---
+++ experiments/basic/basic.py
@@ -0,0 +1,68 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import itertools
+import os
+
+from lab.environments import LocalEnvironment
+# from lab.reports import Attribute, geometric_mean
+# from downward.reports.compare import ComparativeReport
+import common_setup
+from common_setup import IssueConfig, IssueExperiment
+
+DIR = os.path.dirname(os.path.abspath(__file__))
+SCRIPT_NAME = os.path.splitext(os.path.basename(__file__))[0]
+BENCHMARKS_DIR = os.environ["DOWNWARD_BENCHMARKS"]
+REVISIONS = ["loop"]
+BUILDS = ["release"]
+
+CONFIG_NICKS = []
+
+CONFIG_NICKS.append(('basic', ["--translate-options", '--translate', '--keep-unimportant-variables']))
+
+CONFIGS = [
+    IssueConfig(
+        config_nick,
+        config,
+        build_options=[build],
+        driver_options=["--build", build])
+    for build in BUILDS
+    for config_nick, config in CONFIG_NICKS
+]
+
+SUITE = common_setup.DEFAULT_OPTIMAL_SUITE
+
+
+ENVIRONMENT = LocalEnvironment(processes=48)
+
+exp = IssueExperiment(
+    revisions=REVISIONS,
+    configs=CONFIGS,
+    environment=ENVIRONMENT,
+    time_limit="30m",       # this soft-kills running executable
+    memory_limit="3584M"
+)
+exp.set_property("planner_time_limit", 1800)     # pass this to executable
+exp.set_property("planner_memory_limit", "3.5g")
+
+exp.add_suite(BENCHMARKS_DIR, SUITE)
+
+exp.add_parser(exp.EXITCODE_PARSER)
+exp.add_parser(exp.TRANSLATOR_PARSER)
+exp.add_parser(exp.SINGLE_SEARCH_PARSER)
+exp.add_parser(exp.PLANNER_PARSER)
+
+exp.add_step('build', exp.build)
+exp.add_step('start', exp.start_runs)
+exp.add_step("parse", exp.parse)
+
+exp.add_fetcher(name='fetch')
+
+
+attributes = IssueExperiment.DEFAULT_TABLE_ATTRIBUTES
+# exp.add_comparison_table_step(attributes=attributes)
+exp.add_absolute_report_step(attributes=attributes)
+
+exp.run_steps()
+
+
