1d5694e467677bccbbae7052a2a39f270ea41553
Michael Katz <michael.katz1@ibm.com>
2024-10-23 15:44:07 -0400

    parsing translator output for scoping

--- experiments/sorted/parser.py
+++ experiments/sorted/parser.py
@@ -4,7 +4,9 @@
 
 import os
 import json
+import re
 import shutil
+import ast
 
 from lab.parser import Parser
 
@@ -32,8 +34,42 @@ def sas_name(content, props):
         # props["sas_name"] = os.path.join(path, target_sas_file)
         shutil.copyfile(target_sas_path, props["sas_name"])
 
+
+def parse_statistics(content, props):
+    """Parse all translator output of the following form:
+
+    Translator xxx: yyy
+
+    """
+    pattern = re.compile(r"^Translator (.+): (\d+)(?: KB|)$", flags=re.M)
+    for name, count in pattern.findall(content):
+        attr = name.lower().replace(" ", "_")
+        # Support strings, numbers, tuples, lists, dicts, Booleans, and None.
+        props[f"translator_{attr}"] = ast.literal_eval(count)
+
+def parse_translator_timestamps(content, props):
+    """Parse all translator output of the following forms:
+
+        Computing fact groups: [0.000s CPU, 0.004s wall-clock]
+        Writing output... [0.000s CPU, 0.001s wall-clock]
+
+    The last line reads:
+
+        Done! [6.860s CPU, 6.923s wall-clock]
+
+    """
+    pattern = re.compile(
+        r"^(.+)(?:\.\.\.|:|!) \[(.+)s CPU, .+s wall-clock\]$", flags=re.M
+    )
+    for section, time in pattern.findall(content):
+        section = section.lower().replace(" ", "_")
+        props[f"translator_time_{section}"] = float(time)
+
+
 class SaSParser(Parser):
     def __init__(self):
         Parser.__init__(self)
 
         self.add_function(sas_name, file="static-properties")
+        self.add_function(parse_translator_timestamps)
+        self.add_function(parse_statistics)
