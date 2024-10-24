6ebdc5bf8def3020da8486efa386cef73a9255bf
Michael Katz <michael.katz1@ibm.com>
2024-10-24 20:42:49 +0000

    first results

--- experiments/sorted/fix_properties.py
+++ experiments/sorted/fix_properties.py
@@ -4,11 +4,11 @@ import json
 import sys
 
 if __name__ == "__main__":
-    with open(sys.args[1]) as f:
+    with open(sys.argv[1]) as f:
         data = json.load(f)
     for _, e in data.items():
         if e["problem"].endswith(".pddl"):
-            e["problem"].replace(".pddl", ".sas")
+            e["problem"] = e["problem"].replace(".pddl", ".sas")
 
-    with open(sys.args[1], "w") as f:
-        json.dump(data, f)
+    with open(sys.argv[1], "w") as f:
+        json.dump(data, f, indent=4)
